476bc73316635f08ba53e0aa3b26a9a47b558642
Vylkhin <Vylkhin@live.fr>
2020-04-09 16:34:27 +0200

    Ajout getters CApiculteur

--- src/Entity/CApiculteur.php
+++ src/Entity/CApiculteur.php
@@ -61,37 +61,58 @@ class CApiculteur
      */
     private $type_user;
 
+#=====================GETTERS==========================#
     public function getId(): ?int
     {
         return $this->idApi;
     }
 
-    public function EditApi(string $newName, string $newPrenom, string $newMail, string $newMdp, string $newTel, string $newCodePostal, string $newVille, string $newAddr): ?int{
-      try{
-        if ($newNom != "") $nom = $newNom;
-        if ($newPrenom != "") $prenom = $newPrenom;
-        if ($newMail != "") $mail = $newMail;
-        if ($newMdp != "") $mdp = $newMdp;
-        if ($newTel != "") $tel = $newTel;
-        if ($newCodePostal != "") $code_postal = $newCodePostal;
-        if ($newVille != "") $ville = $newVille;
-        if ($newAddr != "") $post_addr = $newAddr;
-        return 1;
-      } catch (Exception $e){ return 0; }
+    public function getNom(): ?string
+    {
+      return $this->nom;
     }
 
-    public function Connexion(): ?int{
-
+    public function getPrenom(): ?string
+    {
+      return $this->prenom;
     }
 
-    public function Deconnexion(): ?int{
-
+    public function getMail(): ?string
+    {
+      return $this->mail;
     }
 
-    public function getTypeUser(): ?int
+    public function getMdp(): ?string
+    {
+      return $this->mdp;
+    }
+
+    public function getTel(): ?string
+    {
+      return $this->tel;
+    }
+
+    public function getCode_postal(): ?string
+    {
+      return $this->code_postal;
+    }
+
+    public function getVille(): ?string
+    {
+      return $this->ville;
+    }
+
+    public function getPost_addr(): ?string
+    {
+      return $this->post_addr;
+    }
+
+    public function getType_user(): ?int
     {
         return $this->type_user;
     }
+
+#==============================SETTERS=========================#
 
     public function setName(string $nom): self
     {
@@ -154,6 +175,30 @@ class CApiculteur
         $this->type_user = $type_user;
 
         return $this;
+    }
+
+#==================================OTHER FUNCTIONS================================#
+
+    public function EditApi(string $newName, string $newPrenom, string $newMail, string $newMdp, string $newTel, string $newCodePostal, string $newVille, string $newAddr): ?int{
+      try{
+        if ($newNom != "") $nom = $newNom;
+        if ($newPrenom != "") $prenom = $newPrenom;
+        if ($newMail != "") $mail = $newMail;
+        if ($newMdp != "") $mdp = $newMdp;
+        if ($newTel != "") $tel = $newTel;
+        if ($newCodePostal != "") $code_postal = $newCodePostal;
+        if ($newVille != "") $ville = $newVille;
+        if ($newAddr != "") $post_addr = $newAddr;
+        return 1;
+      } catch (Exception $e){ return 0; }
+    }
+
+    public function Connexion(): ?int{
+
+    }
+
+    public function Deconnexion(): ?int{
+
     }
 
 }
