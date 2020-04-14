e127d24008159d51a45936d49c4e1fbe8c459c46
clara <clara.krchr@gmail.com>
2020-04-14 14:39:56 +0200

    Form + bdd

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,7 +6,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
+use App\Entity\CApiculteur;
 use App\Form\RegistrationFormType;
 
 class RegistrationController extends AbstractController
@@ -14,8 +16,30 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/registration", name="registration")
      */
-    public function new(EntityManagerInterface $em) {
+    public function new(EntityManagerInterface $em, Request $request) {
         $form = $this->createForm(RegistrationFormType::class);
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $CApiculteur = new CApiculteur();
+            
+            $CApiculteur->setName($data['Nom']);
+            $CApiculteur->setPrenom($data['Prenom']);
+            $CApiculteur->setMail($data['Adresse_mail']);
+            $CApiculteur->setMdp($data['Mot_de_passe']);
+            $CApiculteur->setTel($data['Telephone']);
+            $CApiculteur->setCodePostal($data['Code_postal']);
+            $CApiculteur->setVille($data['Ville']);
+            $CApiculteur->setPostAddr($data['Adresse_postale']);
+            $CApiculteur->setTypeUser(0);
+            
+            $em->persist($CApiculteur);
+            $em->flush();
+            
+            return $this->redirectToRoute('index');
+        }
         
         return $this->render('registration/new.html.twig', [
             'registrationForm' => $form->createView(),
