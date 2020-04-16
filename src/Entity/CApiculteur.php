483e0dd722c812c7af5a16c695b61e922c90a7eb
clara <clara.krchr@gmail.com>
2020-04-16 16:44:26 +0200

    login

--- src/Entity/CApiculteur.php
+++ src/Entity/CApiculteur.php
@@ -201,5 +201,52 @@ class CApiculteur implements UserInterface
     public function Deconnexion(): ?int{
 
     }
+    
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+    /**
+    * @see UserInterface
+    */
+    public function getPassword()
+    {
+        // not needed for apps that do not check user passwords
+    }
+    /**
+    * @see UserInterface
+    */
+    public function getSalt()
+    {
+        // not needed for apps that do not check user passwords
+    }
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 
 }
