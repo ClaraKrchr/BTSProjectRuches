cc5567ed1fa40e9fd52d61282a9689cdc7afe5b0
clara <clara.krchr@gmail.com>
2020-04-16 16:36:06 +0200

    Connexion

--- src/Entity/CApiculteur.php
+++ src/Entity/CApiculteur.php
@@ -3,11 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CApiculteurRepository")
  */
-class CApiculteur
+class CApiculteur implements UserInterface
 {
     /**
      * @ORM\Id()
