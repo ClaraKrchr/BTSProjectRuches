a595fa4e01eeab0e2da57c6c407ac780a014a939
Clement <clement.esquirol@lycee-bourelle.fr>
2020-04-18 12:25:20 +0200

    Page de chaque Region de la carte

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -37,18 +37,93 @@ class MapController extends NouvellepageController{
         return $this->render('map/normandie.html.twig');
     }
     
+    /**
+     * @Route("/corse", name="corse")
+     */
+    public function corse()
+    {
+        return $this->render('map/corse.html.twig');
+    }
     
+    /**
+     * @Route("/occitanie", name="occitanie")
+     */
+    public function occitanie()
+    {
+        return $this->render('map/occitanie.html.twig');
+    }
     
+    /**
+     * @Route("/nouvelle_aquitaine", name="nouvelle_aquitaine")
+     */
+    public function nouvelle_aquitaine()
+    {
+        return $this->render('map/nouvelle_aquitaine.html.twig');
+    }
     
+    /**
+     * @Route("/provence_alpes_cote_dazur", name="provence_alpes_cote_dazur")
+     */
+    public function provence_alpes_cote_dazur()
+    {
+        return $this->render('map/provence_alpes_cote_dazur.html.twig');
+    }
     
+    /**
+     * @Route("/ile_de_france", name="ile_de_france")
+     */
+    public function ile_de_france()
+    {
+        return $this->render('map/ile_de_france.html.twig');
+    }
     
+    /**
+     * @Route("/pays_de_la_loire", name="pays_de_la_loire")
+     */
+    public function pays_de_la_loire()
+    {
+        return $this->render('map/pays_de_la_loire.html.twig');
+    }
     
+    /**
+     * @Route("/grand_est", name="grand_est")
+     */
+    public function grand_est()
+    {
+        return $this->render('map/grand_est.html.twig');
+    }
     
+    /**
+     * @Route("/hauts_de_france", name="hauts_de_france")
+     */
+    public function hauts_de_france()
+    {
+        return $this->render('map/hauts_de_france.html.twig');
+    }
     
+    /**
+     * @Route("/auvergne_rhone_alpes", name="auvergne_rhone_alpes")
+     */
+    public function auvergne_rhone_alpes()
+    {
+        return $this->render('map/auvergne_rhone_alpes.html.twig');
+    }
     
+    /**
+     * @Route("/centre_val_de_loire", name="centre_val_de_loire")
+     */
+    public function centre_val_de_loire()
+    {
+        return $this->render('map/centre_val_de_loire.html.twig');
+    }
     
-    
-    
+    /**
+     * @Route("/bourgogne_franche_comte", name="bourgogne_franche_comte")
+     */
+    public function bourgogne_franche_comte()
+    {
+        return $this->render('map/bourgogne_franche_comte.html.twig');
+    }
     
     
     
