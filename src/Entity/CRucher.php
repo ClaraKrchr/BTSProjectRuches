38c77d500d2c0a655d6c1c959d2d82d126516eed
Vylkhin <Vylkhin@live.fr>
2020-04-23 15:52:17 +0200

    crucher modifications

--- src/Entity/CRucher.php
+++ src/Entity/CRucher.php
@@ -26,6 +26,21 @@ class CRucher
      */
     private $nb_ruches;
 
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $latitude;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $longitude;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $nom;
+
 #=======================GETTERS========================#
 
     public function getId(): ?int
@@ -43,6 +58,21 @@ class CRucher
         return $this->nb_ruches;
     }
 
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
 #========================SETTERS=======================#
 
     public function setLocalisation(string $localisation): self
@@ -55,6 +85,27 @@ class CRucher
     public function setNbRuches(?int $nb_ruches): self
     {
         $this->nb_ruches = $nb_ruches;
+
+        return $this;
+    }
+
+    public function setLatitude(float $latitude): self
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function setLongitude(float $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
 
         return $this;
     }
