7b39e238dc67487172a85698450d9fdae6ae21f9
EsquirolClement <clement.esquirol@lycee-bourdelle.fr>
2020-04-24 11:58:31 +0200

    Liste deroulante add rucher

--- src/Form/AddRucherForm.php
+++ src/Form/AddRucherForm.php
@@ -3,13 +3,33 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 class AddRucherFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-        ->add('Region')
+        ->add('Region',
+            ChoiceType::class,
+            array(
+                'choices'=>array(
+                    ''=>'',
+                    'Auvergne-Rhones-Alpes'=>'Auvergne-Rhones-Alpes',
+                    'Bourgogne-Franche-Comte'=>'Bourgogn-Franche-Comte',
+                    'Bretagne' =>'Bretagne',
+                    'Centre-Val-de-Loire'=>'Centre-Val-de-Loire',
+                    'Corse'=>'Corse',
+                    'Grand-Est'=>'Grand-Est',
+                    'Hauts-de-France'=>'Hauts-de-France',
+                    'Ile-de-France'=>'Ile-de-France',
+                    'Normandie'=>'Normandie',
+                    'Nouvelle-Aquitaine'=>'Nouvelle-Aquitaine',
+                    'Occitanie'=>'Occitanie',
+                    'Pays-de-la-Loire'=>'Pays-de-la-Loire',
+                    'Provence-Alpes-Cotes-d-Azur'=>'Provence-Alpes-Cotes-d-Azur')
+                )
+            )
         ->add('Nb_Ruche')
         ->add('Latitude')
         ->add('Longitude')
