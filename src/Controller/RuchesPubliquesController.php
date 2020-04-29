3a56d099642920bfe061a4b1f108f07c9ab2aeef
elyot <elyot@DESKTOP-1LCKN8A>
2020-04-29 14:52:57 +0200

    Ruches publiques

--- src/Controller/RuchesPubliquesController.php
+++ src/Controller/RuchesPubliquesController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\CPeseRuche;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
 use App\Form\RuchesPubliquesFormType;
+use App\Repository\CPeseRucheRepository;
 
 class RuchesPubliquesController extends AbstractController
 {
@@ -15,10 +17,11 @@ class RuchesPubliquesController extends AbstractController
      */
     public function new(EntityManagerInterface $em)   {
         
-        //die('Todo!');
-        $form = $this->createForm(RuchesPubliquesFormType::class);
+        $pubPeseRuches = $this->getDoctrine()->getRepository(CPeseRuche::class)->findBy(array('visibilite'=>1));
+        
+        $form = $this->createForm(\App\Form\RuchesPubliquesFormType::class);
         return $this->render('ruches_publiques/new.html.twig',
-            ['filterForm' => $form->createView(),
+            ['filterForm' => $form->createView(),'pubPeseRuches' =>$pubPeseRuches,
             ]);
     }
 }
--- src/Form/RuchesPubliquesFormType.php
+++ src/Form/RuchesPubliquesFormType.php
@@ -5,14 +5,44 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RuchesPubliquesFormType extends AbstractType {
     
     public function buildForm(FormBuilderInterface $builder, array $options){
         
         $builder
-        ->add('nomPropio')
-        ->add('nomRuche')
+        ->add('Proprietaire')
+        ->add('Nom_ruche')
+        ->add('Type_T')
+        ->add('Visibilite')
+        ->add('Type_C', ChoiceType::class, [
+            'choices'=> [
+                ''=>'',
+                'Type1' => 'type1',
+                'Type2' => 'type2',
+                'Type3' => 'type3',
+                'Type4' => 'type4',
+            ]
+        ])
+        ->add('Region', ChoiceType::class, [
+            'choices'=> [
+                ''=>'',
+                'Auvergne-Rhones-Alpes'=>'Auvergne-Rhones-Alpes',
+                'Bourgogne-Franche-Comte'=>'Bourgogn-Franche-Comte',
+                'Bretagne' =>'Bretagne',
+                'Centre-Val-de-Loire'=>'Centre-Val-de-Loire',
+                'Corse'=>'Corse',
+                'Grand-Est'=>'Grand-Est',
+                'Hauts-de-France'=>'Hauts-de-France',
+                'Ile-de-France'=>'Ile-de-France',
+                'Normandie'=>'Normandie',
+                'Nouvelle-Aquitaine'=>'Nouvelle-Aquitaine',
+                'Occitanie'=>'Occitanie',
+                'Pays-de-la-Loire'=>'Pays-de-la-Loire',
+                'Provence-Alpes-Cotes-d-Azur'=>'Provence-Alpes-Cotes-d-Azur'
+            ]
+        ])
         ;
-    }   
+    }    
 }
