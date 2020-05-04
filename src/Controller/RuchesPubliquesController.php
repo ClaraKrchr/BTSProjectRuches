034ff6eaf94ec7d824bf8c81f986e07e410ffd7e
Unever358 <elyott.djafour@hotmail.com>
2020-05-04 17:49:47 +0200

    Pagination ruches_publiques

--- src/Controller/RuchesPubliquesController.php
+++ src/Controller/RuchesPubliquesController.php
@@ -7,21 +7,31 @@ use App\Entity\CRucher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 use App\Form\RuchesPubliquesFormType;
 use App\Repository\CPeseRucheRepository;
 
 class RuchesPubliquesController extends AbstractController{
     /**
-     * @Route("/ruches/publiques/{regions}", name="ruches_publiques")
+     * @Route("/ruches/publiques/{regions}/{page}", name="ruches_publiques", defaults={"page"=1})
      */
-    public function new(EntityManagerInterface $em ,$regions)   {
+    public function new(EntityManagerInterface $em, CPeseRucheRepository $repository, $regions, PaginatorInterface $paginator, Request $request, $page)   {
         
         $ruchers = $this->getDoctrine()->getRepository(CRucher::class)->findBy(array('region'=>$regions));
-        $pubPeseRuches = $this->getDoctrine()->getRepository(CPeseRuche::class)->findBy(array('rucher'=>$ruchers,'visibilite'=>0));
+        $paginations = $paginator->paginate(
+            $repository->findBy(array('rucher'=>$ruchers,'visibilite'=>0)),
+            $page,
+            6
+            );
+        
+        //$pubPeseRuches = $this->getDoctrine()->getRepository(CPeseRuche::class)->findBy(array('rucher'=>$ruchers,'visibilite'=>0));
         $form = $this->createForm(\App\Form\RuchesPubliquesFormType::class);
         return $this->render('ruches_publiques/ruches_publiques.html.twig',
-            ['filterForm' => $form->createView(),'pubPeseRuches' =>$pubPeseRuches,
+            ['filterForm' => $form->createView(),
+                //'pubPeseRuches' =>$pubPeseRuches,
+                'paginations' => $paginations
             ]);
     }
 }
--- src/Form/RuchesPubliquesFormType.php
+++ src/Form/RuchesPubliquesFormType.php
@@ -7,42 +7,30 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-class RuchesPubliquesFormType extends AbstractType {
-    
-    public function buildForm(FormBuilderInterface $builder, array $options){
+class RuchesPubliquesFormType extends AbstractType 
+{    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         
-        $builder
-        ->add('Proprietaire')
+        $builder        
         ->add('Nom_ruche')
-        ->add('Type_T')
-        ->add('Visibilite')
-        ->add('Type_C', ChoiceType::class, [
+        ->add('Proprietaire')
+        ->add('Type',ChoiceType::class, [
             'choices'=> [
-                ''=>'',
-                'Type1' => 'type1',
-                'Type2' => 'type2',
-                'Type3' => 'type3',
-                'Type4' => 'type4',
-            ]
-        ])
-        ->add('Region', ChoiceType::class, [
-            'choices'=> [
-                ''=>'',
-                'Auvergne-Rhones-Alpes'=>'Auvergne-Rhones-Alpes',
-                'Bourgogne-Franche-Comte'=>'Bourgogn-Franche-Comte',
-                'Bretagne' =>'Bretagne',
-                'Centre-Val-de-Loire'=>'Centre-Val-de-Loire',
-                'Corse'=>'Corse',
-                'Grand-Est'=>'Grand-Est',
-                'Hauts-de-France'=>'Hauts-de-France',
-                'Ile-de-France'=>'Ile-de-France',
-                'Normandie'=>'Normandie',
-                'Nouvelle-Aquitaine'=>'Nouvelle-Aquitaine',
-                'Occitanie'=>'Occitanie',
-                'Pays-de-la-Loire'=>'Pays-de-la-Loire',
-                'Provence-Alpes-Cotes-d-Azur'=>'Provence-Alpes-Cotes-d-Azur'
-            ]
-        ])
-        ;
+            'Ruches en paille'=>'Ruches en paille',
+            'Ruche kenyane(KTBH)'=>'Ruche kenyane(KTBH)',
+            'Ruche Tronc'=>'Ruche Tronc',
+            'Ruche alsacienne(Ruche Bastian)'=>'Ruche alsacienne(Ruche Bastian)',
+            'Ruche horizontales'=>'Ruche horizontales',
+            'Ruche Dadant'=>'Ruche Dadant',
+            'Ruche Langstroth(standard)'=>'Ruche Langstroth(standard)',
+            'Ruche Voirnot'=>'Ruche Voirnot',
+            'Ruche Warre(ruche populaire)'=>'Ruche Warre(ruche populaire)',
+            'Ruche Layens'=>'Ruche Layens',
+            'Ruche William Braughton Carr'=>'Ruche William Braughton Carr',
+            'Ruche de production'=>'Ruche de production',
+            ],
+            'required'=> false
+        ]);        
     }    
 }
