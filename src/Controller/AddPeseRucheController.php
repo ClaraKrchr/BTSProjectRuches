ab195953e80e94f5a866f71b962879f129171e1c
ClaraKrchr <clara.krchr@gmail.com>
2020-05-09 17:15:49 +0200

    association

--- src/Controller/AddPeseRucheController.php
+++ src/Controller/AddPeseRucheController.php
@@ -8,6 +8,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\CPeseRuche;
+use App\Entity\AssociationPeserucheStation;
+use App\Entity\AssociationRuchePeseruche;
+use App\Entity\CStation;
+
 use App\Form\AddPeseRucheFormType;
 
 class AddPeseRucheController extends AbstractController
@@ -15,15 +19,37 @@ class AddPeseRucheController extends AbstractController
     /**
      * @Route("/add_pese_ruche", name="add_pese_ruche")
      */
-    public function new(EntityManagerInterface $em, Request $request)
+    public function add_pese_ruche(EntityManagerInterface $em, Request $request)
     {
-        $CPeseRuche = new CPeseRuche();
-        $form = $this->createForm(AddPeseRucheFormType::class, $CPeseRuche);
+         
+        
+        $form = $this->createForm(AddPeseRucheFormType::class);
         $form->handleRequest($request);
         
         if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            
+            $CPeseRuche = new CPeseRuche();
+            $CPeseRuche->setNomPeseRuche($data['nompeseruche']);
+            $CPeseRuche->setDateInstall($data['dateinstall']);
+            $CPeseRuche->setVisibilite($data['visibilite']);
+            
             $em->persist($CPeseRuche);
+            
+            $associationPeserucheStation = new AssociationPeserucheStation();
+            
+            $associationPeserucheStation->setPeseruche($CPeseruche);
+            $associationPeserucheStation->setStation($em->getRepository(CStation::class)->findOneBy(array('id'=>($data['nomstation'])->getId())));
+            $em->persist($associationPeserucheStation);
+            ($data['nomstation'])->addAssociationPeserucheStation($associationPeserucheStation);
+                        
+            $CPeseRuche->setAssociationPeserucheStation($associationPeserucheStation);           
             $em->flush();
+            
+            $message=utf8_encode('Le pèse ruche a été ajoutée');
+            $this->addFlash('message',$message);
+            
+            return $this->redirectToRoute('add_pese_ruche');
         }
         
         
--- src/Form/AddPeseRucheFormType.php
+++ src/Form/AddPeseRucheFormType.php
@@ -3,10 +3,14 @@
 namespace App\Form;
 
 use App\Entity\CPeseRuche;
+use App\Entity\CStation;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AddPeseRucheFormType extends AbstractType
 {
@@ -18,15 +22,21 @@ class AddPeseRucheFormType extends AbstractType
                 'widget' => 'single_text',
                 
             ])
-            ->add('associationPeserucheStation')
-            ->add('associationRuchePeseruche')
+            ->add('visibilite',ChoiceType::class,
+                array(
+                    'choices'=>array(
+                        'Publique'=>'0',
+                        'Privee'=>'1',
+                    ),
+                    'expanded'=>true,
+                    'multiple'=>false
+                ))                
+             ->add('nomstation',EntityType::class, [
+                'class'=>CStation::class,
+                'choice_label'=>function(CStation $CStation){
+                return sprintf(' %s',$CStation->getNom());
+                }
+                ])   
         ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => CPeseRuche::class,
-        ]);
     }
 }
