015e5955f7d5b7ea201074e42a3e32add4fcb862
EsquirolClement <clement.esquirol@lycee-bourdelle.fr>
2020-05-10 15:42:24 +0200

    Affichage ruche

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,28 +2,37 @@
 
 namespace App\Controller;
 
-use App\Entity\CPeseRuche;
-use App\Entity\CApiculteur;
-use App\Entity\CRucher;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 use App\Entity\CRuche;
+use App\Entity\CRucher;
+use App\Entity\AssociationRucheRucher;
+use App\Entity\AssociationRucheApiculteur;
 
 class MapController extends NouvellepageController{
         
     /**
+     * @IsGranted("ROLE_USER")
      * @Route("/tableau_donnees/{regions}",name="tableau_donnees")
      */
     public function tableau_donnees($regions)
     {
+        //--------Obtention du nom ce l'utilisateur----------------//
         $NomProprietaire=$this->getUser();
-        $ruchers = $this->getDoctrine()->getRepository(CRucher::class)->findBy(array('region'=>$regions));
-        $ruches = $this->getDoctrine()->getRepository(CRuche::class)->findBy(array('rucher'=>$ruchers,'proprietaire'=>$NomProprietaire));
-               
-        return $this->render('map/tableau_donnees.html.twig', ['ruches' => $ruches,'region'=>$regions]);
+        //-------------Recherche des ruchers dans la region---------------//
+        $Ruchers = $this->getDoctrine()->getRepository(CRucher::class)->findBy(array('region'=>$regions));
+        //-------------Recherche des ruches dans les ruchers-----------------//
+        $RuchesRuchers= $this->getDoctrine()->getRepository(AssociationRucheRucher::class)->findBy(array('rucher'=>$Ruchers));
+        //------------Recherche des ruches appartenant a l'utilisateur connecté-------------//
+        $RuchesApiculteurs = $this->getDoctrine()->getRepository(AssociationRucheApiculteur::class)->findBy(array('ruche'=>$RuchesRuchers,'apiculteur'=>$NomProprietaire));
+       
+        return $this->render('map/tableau_donnees.html.twig', ['apiculteurs' => $RuchesApiculteurs,'region'=>$regions]);
     }
     
     /**
