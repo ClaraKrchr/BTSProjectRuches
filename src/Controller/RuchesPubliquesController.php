e0b6694f5250133f21ad1007875b8b81848757bb
Unever358 <elyott.djafour@hotmail.com>
2020-05-12 15:50:22 +0200

    ruches_publiques Associations

--- src/Controller/RuchesPubliquesController.php
+++ src/Controller/RuchesPubliquesController.php
@@ -2,40 +2,48 @@
 
 namespace App\Controller;
 
-use App\Entity\CPeseRuche;
-
-use App\Entity\CApiculteur;
 use App\Entity\CRucher;
+use App\Entity\CRuche;
+use App\Entity\AssociationRucheRucher;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Knp\Component\Pager\PaginatorInterface;
+use App\Repository\CRucheRepository;
 
 use App\Form\RuchesPubliquesFormType;
-use App\Repository\CPeseRucheRepository;
 
 class RuchesPubliquesController extends AbstractController{
     /**
      * @Route("/ruches/publiques/{regions}/{page}", name="ruches_publiques", defaults={"page"=1})
      */
-    public function new(EntityManagerInterface $em, CPeseRucheRepository $repository, $regions, PaginatorInterface $paginator, Request $request, $page)   {
+    public function new(EntityManagerInterface $em, $regions, PaginatorInterface $paginator, Request $request, $page)   {
         
         $ruchers = $this->getDoctrine()->getRepository(CRucher::class)->findBy(array('region'=>$regions));
-              
-        //$pubPeseRuches = $this->getDoctrine()->getRepository(CPeseRuche::class)->findBy(array('rucher'=>$ruchers,'visibilite'=>0));
+        $AssosRucheRucher = $this->getDoctrine()->getRepository(AssociationRucheRucher::class)->findBy(array('rucher'=>$ruchers));
+        $ARRLength = count($AssosRucheRucher);
+        for($i = 0; $i < $ARRLength; $i++)
+        {
+            $j = 0;
+            if ($AssosRucheRucher[$i]->getRuche()->getVisibilite() == '0'){
+                $ruches[$j] = $AssosRucheRucher[$i]->getRuche();
+                $j++;
+            }
+        }
         $form = $this->createForm(\App\Form\RuchesPubliquesFormType::class);
         
         
         $paginations = $paginator->paginate(
-            $repository->findBy(array('visibilite'=>0,'rucher'=>$ruchers)),
+            $ruches,
             $page,
             6
             );  
-        return $this->render('ruches_publiques/ruches_publiques.html.twig',
+        return $this->render('Ruches/ruches_publiques.html.twig',
             ['filterForm' => $form->createView(),
-                //'pubPeseRuches' =>$pubPeseRuches,
-                'paginations' => $paginations
+                'paginations' => $paginations,
+                'region' => $regions
             ]);
     }
 }
