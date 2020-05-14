8d0bec7ed976bd66a8f941139c4f9d05bc95291b
Vylkhin <Vylkhin@live.fr>
2020-05-14 16:00:21 +0200

    Suppression addflash googleMap

--- src/Controller/NouvellepageController.php
+++ src/Controller/NouvellepageController.php
@@ -37,9 +37,7 @@ class NouvellepageController extends AbstractController
      * @Route("/googleMap", name="googleMap")
      */
     public function googleMap(){
-        $message=utf8_encode('Pour ajouter un rucher, récupérez les coordonnées en cliquant sur la carte puis copiez-les et cliquer sur le bouton "Ajouter un rucher"');
         $ruchers = $this->getDoctrine()->getRepository(CRucher::class)->findAll();
-        $this->addFlash('messageGoogleMap',$message);
         return $this->render('map/googleMap.html.twig', ['ruchers' => $ruchers,]);
     }
     /**
