9fe93974bdb31b5c892d268bff869b646a9c0688
Vylkhin <Vylkhin@live.fr>
2020-05-15 16:25:39 +0200

    Début edit Ruche
    Marche pas car "Pas de propriété Rucher dans CRuche"

---
+++ src/Controller/EditController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CRuche;
+use App\Entity\AssociationRuchePeseruche;
+use App\Entity\AssociationRucheRucher;
+use App\Entity\CPeseRuche;
+use App\Entity\CRucher;
+
+use App\Form\EditRucheType;
+use App\Repository\CRucheRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+class EditController extends AbstractController
+{
+    /**
+     * @IsGranted("ROLE_USER")
+     * @Route("/tableau_donnees/edit/{nomruche}", name="edit_ruche")
+     */
+    public function editRuche(Request $request, CRuche $ruche, EntityManagerInterface $em)
+    {
+        
+        $form = $this->createForm(EditRucheType::class, $ruche);
+        
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $ruche->setNomruche($data['Nom_ruche']);
+            $ruche->setDateinstall($data['Date_installation']);
+            $ruche->setVisibilite($data['Visibilite']);
+            $ruche->setTyperuche($data['Type_ruche']);
+            $ruche->setEtat($data['Etat']);
+            
+            $em->persist($ruche);
+            
+            if($data['Rucher'] != NULL){
+                
+                if ($em->getRepository(AssociationRucheRucher::class)->findOneBy(array('ruche'=>$ruche, 'rucher'=>($data['Rucher'])))){}
+                else{
+                    $AssociationRucheRucher = new AssociationRucheRucher;
+                    $AssociationRucheRucher->setRuche($CRuche);
+                    $AssociationRucheRucher->setRucher($em->getRepository(CRucher::class)->findOneBy(array('id'=>($data['Rucher'])->getId())));
+                    $em->persist($AssociationRucheRucher);
+                    ($data['Rucher'])->addAssociationRucheRucher($AssociationRucheRucher);
+                    $CRuche->setAssociationRucheRucher($AssociationRucheRucher);
+                }
+            }
+            
+            if($data['PeseRuche'] != NULL){
+                
+                if ($em->getRepository(AssociationRuchePeseruche::class)->findOneBy(array('ruche'=>$ruche, 'peseruche'=>($data['PeseRuche'])))){}
+                else{
+                    $AssociationRuchePeseruche = new AssociationRuchePeseruche();
+                    
+                    $AssociationRuchePeseruche->setRuche($ruche);
+                    $AssociationRuchePeseruche->setPeseruche($em->getRepository(CPeseRuche::class)->findOneBy(array('id'=>($data['PeseRuche'])->getId())));
+                    $em->persist($AssociationRuchePeseruche);
+                    ($data['PeseRuche'])->setAssociationRuchePeseruche($AssociationRuchePeseruche);
+                    $ruche->setAssociationRuchePeseruche($AssociationRuchePeseruche);
+                }
+            }
+            
+            $em->flush();
+            
+            return $this->redirectToRoute('tableau_donnees');
+        }
+        return $this->render('Ruches/editRuche.html.twig', ['formRuche' => $form->createView()]);
+    }
+}
---
+++ src/Form/EditRucheType.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CRuche;
+use App\Entity\CRucher;
+use App\Entity\CPeseRuche;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class EditRucheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Nom_ruche')
+            ->add('Date_install',DateType::class, [
+                'widget' => 'single_text',
+                
+            ])
+            ->add('Etat',ChoiceType::class,
+                array(
+                    'choices'=>array(
+                        'En attente'=>'0',
+                        'Dans un rucher'=>'1',
+                        'Chez un apiculteur'=>'2'
+                    )
+                )
+                )
+            ->add('Type_ruche',ChoiceType::class, [
+                'choices'=> [
+                    'Ruches en paille'=>'Ruches en paille',
+                    'Ruche kenyane(KTBH)'=>'Ruche kenyane(KTBH)',
+                    'Ruche Tronc'=>'Ruche Tronc',
+                    'Ruche alsacienne(Ruche Bastian)'=>'Ruche alsacienne(Ruche Bastian)',
+                    'Ruche horizontales'=>'Ruche horizontales',
+                    'Ruche Dadant'=>'Ruche Dadant',
+                    'Ruche Langstroth(standard)'=>'Ruche Langstroth(standard)',
+                    'Ruche Voirnot'=>'Ruche Voirnot',
+                    'Ruche Warre(ruche populaire)'=>'Ruche Warre(ruche populaire)',
+                    'Ruche Layens'=>'Ruche Layens',
+                    'Ruche William Braughton Carr'=>'Ruche William Braughton Carr',
+                    'Ruche de production'=>'Ruche de production',
+                ],
+            ])
+            ->add('Rucher',EntityType::class, [
+                'class'=>CRucher::class,
+                'choice_label'=>function(CRucher $CRucher){
+                return sprintf(' %s',$CRucher->getNom());
+                },
+                'required'=>false
+                ])
+            ->add('PeseRuche',EntityType::class, [
+                'class'=>CPeseRuche::class,
+                'choice_label'=>function(CPeseRuche $CPeseRuche){
+                return sprintf(' %s',$CPeseRuche->getNomPeseRuche());
+                },
+                'required'=>false
+                ])
+            ->add('Visibilite',ChoiceType::class,
+                array(
+                    'choices'=>array(
+                        'Publique'=>'0',
+                        'Privee'=>'1',
+                    ),
+                    'expanded'=>true,
+                    'multiple'=>false
+                ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => CRuche::class,
+        ]);
+    }
+}
