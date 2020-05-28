bc865a0ab99288584c3d61beafb88175d88548f2
EsquirolClement <clement.esquirol@lycee-bourdelle.fr>
2020-05-28 15:56:41 +0200

    Add mesures ruches réparés

--- src/Form/AddMesuresRuchesForm.php
+++ src/Form/AddMesuresRuchesForm.php
@@ -23,7 +23,7 @@ class AddMesuresRuchesForm extends AbstractType
         ->add('ruche',EntityType::class, [
             'class'=>CRuche::class,
             'query_builder' => function(EntityRepository $er){
-            return $er->createQueryBuilder('u')->select('w')->from(CRuche::class, 'w')->orderBy('w.nom', 'ASC');
+            return $er->createQueryBuilder('u')->select('w')->from(CRuche::class, 'w')->orderBy('w.nomruche', 'ASC');
             },
             'choice_label'=>function(CRuche $CRuche){
             return sprintf(' %s',$CRuche->getNomruche());
