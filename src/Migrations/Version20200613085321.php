60220679eb3752c8d1fe89c0efddafc93ee21598
Vylkhin <Vylkhin@live.fr>
2020-06-13 13:49:32 +0200

    Migration sauvetage

---
+++ src/Migrations/Version20200613085321.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200613085321 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mesures_ruches CHANGE poids poids DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE mesures_stations CHANGE tension tension DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mesures_ruches CHANGE poids poids SMALLINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE mesures_stations CHANGE tension tension SMALLINT NOT NULL');
+    }
+}
