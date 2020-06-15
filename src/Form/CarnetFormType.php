041ec8d7f78515a08732a81cd04f2c0a85d43645
Vylkhin <Vylkhin@live.fr>
2020-06-15 13:59:49 +0200

    Listes déroulantes carnet bloquées sur utilisateur actif

--- src/Form/CarnetFormType.php
+++ src/Form/CarnetFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Carnet;
 use App\Entity\CRuche;
+use App\Entity\AssocierRucheApiculteur;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,6 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Doctrine\ORM\EntityRepository;
 
 class CarnetFormType extends AbstractType
 {
@@ -19,11 +21,21 @@ class CarnetFormType extends AbstractType
         $builder
             ->add('ruche',EntityType::class, [
                 'class'=>CRuche::class,
+                'query_builder' => function(EntityRepository $er) use($options){
+                return $er->createQueryBuilder('u')->select('w')->from(CRuche::class, 'w')->join(AssocierRucheApiculteur::class, 'a')->where('w.id = a.ruche AND a.apiculteur = :user')->orderBy('w.nomruche', 'ASC')->setParameter('user', $options['user']);
+                },
                 'choice_label'=>function(CRuche $CRuche){
                 return sprintf(' %s',$CRuche->getNomruche());
                 },
                 'required'=>true
                 ])            
         ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'user' => null,
+        ));
     }
 }
