18f9084cafb6ddb844dd79e692adf4200fde71a0
Vylkhin <Vylkhin@live.fr>
2020-06-17 16:49:24 +0200

    Gestionnaires ruchers et stations

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\CApiculteur;
 use App\Entity\CRucher;
+use App\Entity\CStation;
 use App\Entity\AssocierRucheRucher;
 use App\Entity\AssocierStationRucher;
+use App\Entity\AssocierRuchePort;
+use App\Repository\CStationRepository;
 use App\Repository\CRucherRepository;
 use App\Form\EditUserType;
 use App\Repository\CApiculteurRepository;
@@ -18,6 +21,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class AdminController extends AbstractController
 {
+    
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/gestionnaire_admin", name="gestionnaire_admin")
+     */
+    public function gestionnaire_admin()
+    {
+        return $this->render("admin/gestionnaire_admin.html.twig",);
+    }
+    
     /**
      * @IsGranted("ROLE_ADMIN")
      * @Route("/gestionnaire", name="gestionnaire")
@@ -72,11 +85,52 @@ class AdminController extends AbstractController
         if (($ruche == NULL) && ($station == NULL)){
             $em->remove($rucher);
         }
+        else{
+            $message=utf8_encode('Le rucher n\'est pas vide.');
+            $this->addFlash('message',$message);
+            return $this->redirectToRoute('ruchers');
+        }
         $em->flush();
         
         $message=utf8_encode('Le rucher a été supprimé.');
         $this->addFlash('message',$message);
         return $this->redirectToRoute('ruchers');
+    }
+    
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/stations", name="stations")
+     */
+    public function stationsList(CStationRepository $stations)
+    {
+        return $this->render("admin/stations.html.twig", [
+            'stations' => $stations->findAll(),
+            'assosRuche' => $this->getDoctrine()->getRepository(AssocierRuchePort::class)->findAll(),
+            'assosRucher' => $this->getDoctrine()->getRepository(AssocierStationRucher::class)->findAll()
+        ]);
+    }
+    
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/stations/delete/{nom}", name="delete_station")
+     */
+    public function delete_station(Request $request, CStation $station, EntityManagerInterface $em)
+    {
+        $ruche = $this->getDoctrine()->getRepository(AssocierRuchePort::class)->findBy(array('station'=>$station));
+        if ($ruche == NULL){
+            $em->remove($this->getDoctrine()->getRepository(AssocierStationRucher::class)->findOneBy(array('station'=>$station)));
+            $em->remove($station);
+        }
+        else{
+            $message=utf8_encode('La station n\'est pas vide.');
+            $this->addFlash('message',$message);
+            return $this->redirectToRoute('stations');
+        }
+        $em->flush();
+        
+        $message=utf8_encode('La station a été supprimée.');
+        $this->addFlash('message',$message);
+        return $this->redirectToRoute('stations');
     }
     
 }
